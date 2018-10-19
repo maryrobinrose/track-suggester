69ffd92a43951444ede1c3014a77d24b5e264d82
Mary McMurray <maryrobinrose@gmail.com>
2018-10-19 12:24:31 -0700

    add variable for each question

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
     $("#results").show();
-    var question = $("input:radio[name=q1]:checked").val();
-
+    var question = $("input:radio[name=one]:checked").val();
+    var question = $("input:radio[name=two]:checked").val();
+    var question = $("input:radio[name=three]:checked").val();
+    var question = $("input:radio[name=four]:checked").val();
+    var question = $("input:radio[name=five]:checked").val();
     $("#track").show()
+
 
     event.preventDefault();
 
