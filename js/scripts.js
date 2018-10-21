fd0ef42acbb28b400e2765d519106f34790c8c4a
Mary McMurray <maryrobinrose@gmail.com>
2018-10-21 16:42:50 -0700

    add hide to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
-
+    event.preventDefault();
     var questionOne = $("input:radio[name=one]:checked").val();
     var questionTwo = $("input:radio[name=two]:checked").val();
     var questionThree = $("input:radio[name=three]:checked").val();
@@ -9,16 +9,19 @@ $(document).ready(function(){
 
     if (questionOne === "true" && questionTwo === "true" && questionThree === "true"  && questionFour === "true" && questionFive === "true") {
       $("#trackOne").show();
+      $("#trackTwo").hide();
+      $("#trackThree").hide();
   }
   else if (questionOne != "true" && questionTwo != "true" && questionThree != "true"  && questionFour != "true" && questionFive != "true") {
     $("#trackTwo").show();
+    $("#trackOne").hide();
+    $("#trackThree").hide();
   }
   else {
     $("#trackThree").show();
+    $("#trackTwo").hide();
+    $("#trackOne").hide();
   }
-
-
-    event.preventDefault();
 
 
   });
