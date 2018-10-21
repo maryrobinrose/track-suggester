9e17c3323221d93f9575793daaf80300ccae30c7
Mary McMurray <maryrobinrose@gmail.com>
2018-10-20 20:42:35 -0700

    add if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
     $("#results").show();
-    var question = $("input:radio[name=one]:checked").val();
-    var question = $("input:radio[name=two]:checked").val();
-    var question = $("input:radio[name=three]:checked").val();
-    var question = $("input:radio[name=four]:checked").val();
-    var question = $("input:radio[name=five]:checked").val();
+    var questionOne = $("input:radio[name=one]:checked").val();
+    var questionTwo = $("input:radio[name=two]:checked").val();
+    var questionThree = $("input:radio[name=three]:checked").val();
+    var questionFour = $("input:radio[name=four]:checked").val();
+    var questionFive = $("input:radio[name=five]:checked").val();
 
-    if 
-    $("#track").show()
+    if (questionOne === true && question === true) {
+      $("#track").show()
+    }
+      $("#trackOne").show()
 
 
 
