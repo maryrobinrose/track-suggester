5d242c5c54eff474737c2c33879ddee5efb4c4c2
Mary McMurray <maryrobinrose@gmail.com>
2018-10-21 16:14:20 -0700

    change js variable name and if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
-  
-    var question1 = $("input:radio[name=one]:checked").val();
+
+    var one = $("input:radio[name=one]:checked").val();
     var question2 = $("input:radio[name=two]:checked").val();
     var question3 = $("input:radio[name=three]:checked").val();
     var question4 = $("input:radio[name=four]:checked").val();
     var question5 = $("input:radio[name=five]:checked").val();
 
-    if (question1 === true) {
+    if (one === true) {
       $("#trackOne").show();
       // $("#trackTwo").hide();
       // $("#trackThree").hide();
