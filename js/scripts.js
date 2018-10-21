e9322b22a24ce3286e8218485e3caa8c0f2fe8e7
Mary McMurray <maryrobinrose@gmail.com>
2018-10-21 16:20:47 -0700

    change variable names in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
   $("form#quiz").submit(function(event){
 
     var one = $("input:radio[name=one]:checked").val();
-    var question2 = $("input:radio[name=two]:checked").val();
-    var question3 = $("input:radio[name=three]:checked").val();
-    var question4 = $("input:radio[name=four]:checked").val();
-    var question5 = $("input:radio[name=five]:checked").val();
+    var two = $("input:radio[name=two]:checked").val();
+    var three = $("input:radio[name=three]:checked").val();
+    var four = $("input:radio[name=four]:checked").val();
+    var five = $("input:radio[name=five]:checked").val();
 
     if (one === true) {
       $("#trackOne").show();
-      // $("#trackTwo").hide();
-      // $("#trackThree").hide();
+
+    }
+
+
     // } else if (questionOne === false && questionTwo === false && questionThree === false && questionFour === false && questionFive === false);
     //   $("#trackTwo").show();
     //   $("#trackOne").hide();
