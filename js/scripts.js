961088da31811f2534b8ee05ba2b18d4a636196c
Mary McMurray <maryrobinrose@gmail.com>
2018-10-21 16:32:54 -0700

    change variable names to be more descriptive

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
 
-    var one = $("input:radio[name=one]:checked").val();
-    var two = $("input:radio[name=two]:checked").val();
-    var three = $("input:radio[name=three]:checked").val();
-    var four = $("input:radio[name=four]:checked").val();
-    var five = $("input:radio[name=five]:checked").val();
+    var questionOne = $("input:radio[name=one]:checked").val();
+    var questionTwo = $("input:radio[name=two]:checked").val();
+    var questionThree = $("input:radio[name=three]:checked").val();
+    var questionFour = $("input:radio[name=four]:checked").val();
+    var questionFive = $("input:radio[name=five]:checked").val();
 
-    if (one === "true" && two === "true" && three === "true"  && four === "true" && five === "true") {
+    if (questionOne === "true" && questionTwo === "true" && questionThree === "true"  && questionFour === "true" && questionFive === "true") {
       $("#trackOne").show();
   }
-  else if (one != "true" && two != "true" && three != "true"  && four != "true" && five != "true") {
+  else if (questionOne != "true" && questionTwo != "true" && questionThree != "true"  && questionFour != "true" && questionFive != "true") {
     $("#trackTwo").show();
   }
   else {
